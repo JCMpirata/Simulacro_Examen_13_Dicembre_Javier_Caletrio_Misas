c4bbea5b4f789dfca75450eaa579238d8f194868
JCMpirata <113981043+JCMpirata@users.noreply.github.com>
2022-12-18 11:19:23 +0100

    mas matriz rotate

--- Ejercicio6_ROTATE.py
+++ Ejercicio6_ROTATE.py
@@ -1,1 +1,8 @@
 matriz = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+
+def rotate(matriz):
+    matriz = matriz[::-1]
+    for i in range(len(matriz)):
+        for j in range(i):
+            matriz[i][j], matriz[j][i] = matriz[j][i], matriz[i][j]
+    return matriz

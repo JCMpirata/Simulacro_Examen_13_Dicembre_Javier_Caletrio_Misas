81968358f1d76b84b311c32552c16875aa48d740
JCMpirata <113981043+JCMpirata@users.noreply.github.com>
2022-12-18 14:01:05 +0100

    añadir direccion a la matriz

--- Ejercicio6_ROTATE.py
+++ Ejercicio6_ROTATE.py
@@ -1,11 +1,11 @@
 matriz = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
 
-def rotate(matriz):
-    matriz = matriz[::-1]
-    for i in range(len(matriz)):
-        for j in range(i):
-            matriz[i][j], matriz[j][i] = matriz[j][i], matriz[i][j]
-    return matriz
+def rotate(matriz, direccion):
+    if direccion == "clockwise":
 
-if __name__ == "__main__":
-    print(rotate(matriz))
+        matriz = matriz[::-1]
+        for i in range(len(matriz)):
+            for j in range(i):
+                matriz[i][j], matriz[j][i] = matriz[j][i], matriz[i][j]
+        return matriz
+

c9602b0bb8f8311133778db61e474bd22daad370
JCMpirata <113981043+JCMpirata@users.noreply.github.com>
2022-12-18 14:02:34 +0100

    girar matriz direccion contraria

--- Ejercicio6_ROTATE.py
+++ Ejercicio6_ROTATE.py
@@ -9,3 +9,9 @@ def rotate(matriz, direccion):
                 matriz[i][j], matriz[j][i] = matriz[j][i], matriz[i][j]
         return matriz
 
+    elif direccion == "counter-clockwise":
+        matriz = matriz[::-1]
+        for i in range(len(matriz)):
+            for j in range(i):
+                matriz[i][j], matriz[j][i] = matriz[j][i], matriz[i][j]
+        return matriz

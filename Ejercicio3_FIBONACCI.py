f2474728625d6623ec68d76954fb0f37f031332b
JCMpirata <113981043+JCMpirata@users.noreply.github.com>
2022-12-18 10:49:34 +0100

    mas fibonacci

--- Ejercicio3_FIBONACCI.py
+++ Ejercicio3_FIBONACCI.py
@@ -2,3 +2,22 @@ def Fibonacci(num: int):
     lista = []
     num = _fibonacci(lista, 0, num)
     return lista[-1]
+
+def _fibonacci(lista: list, start: int, end: int):
+    if start == 0 or start == 1:
+        lista.append(start)
+
+    elif 1 < start < end:
+        lista.append(lista[-1] + lista[-2])
+    
+    else:
+        return lista[-1]
+
+    return _fibonacci(lista, start + 1, end)
+
+
+
+
+
+    
+
